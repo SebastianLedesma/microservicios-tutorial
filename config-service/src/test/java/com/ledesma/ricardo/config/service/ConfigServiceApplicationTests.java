664be3b0e13704920ca69c0fa0252cacf8d950fe
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2023-10-01 13:25:22 -0300

    config-service

---
+++ config-service/src/test/java/com/ledesma/ricardo/config/service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledesma.ricardo.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
