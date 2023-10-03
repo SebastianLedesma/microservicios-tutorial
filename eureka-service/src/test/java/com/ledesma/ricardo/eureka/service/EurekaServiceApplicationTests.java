d8b6c993eb7b386b29386d29b6d0b0d243526385
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2023-10-02 23:45:45 -0300

    eureka

---
+++ eureka-service/src/test/java/com/ledesma/ricardo/eureka/service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledesma.ricardo.eureka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
